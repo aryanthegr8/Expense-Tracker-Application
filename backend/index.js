cb2badb5e0a89aa4e542f95becdf3d22abd8375d
aryanthegr8 <er.aryanmathur19@gmail.com>
2025-12-25 22:20:14 +0530

    deployment setup completed

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,12 @@ import mergedTypeDefs from "./typeDefs/index.js";
 
 import { connectDB } from "./db/connectDB.js";
 import { configurePassport } from "./passport/passport.config.js";
+import path from "path";
 
 dotenv.config();
 configurePassport();
 
+const __dirname = path.resolve();
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -73,6 +75,13 @@ app.use(
     context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
+
+// npm run build will build your frontend app, and it will optimized version of your app
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,10 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   // TODO => Update the url on production
-  uri: "http://localhost:4000/graphql",
+  uri:
+    import.meta.env.VITE_NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "/graphql",
   cache: new InMemoryCache(), // apollo client uses to cache query results after fetching them
   credentials: "include", // this tells apollo client to send cookies along with every request to the server
 });
